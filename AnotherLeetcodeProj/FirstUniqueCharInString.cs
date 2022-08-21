0e84a2c550bf33a6e041996e9bbff588586c6cb0
Trevor Larson <trevor.lar@Knights.ucf.edu>
2022-08-21 03:05:35 -0400

    Finished more problems.

--- AnotherLeetcodeProj/FirstUniqueCharInString.cs
+++ AnotherLeetcodeProj/FirstUniqueCharInString.cs
@@ -10,16 +10,86 @@ namespace AnotherLeetcodeProj
     {
         public int FirstUniqChar(string s)
         {
-            int uniquePointer = 0;
-            int movingPointer = 0;
-            HashSet<char> chars = new HashSet<char>();
-
+            Dictionary<char, int> uniqueCharacters = new Dictionary<char, int>();
+            List<int> indexes = new List<int>();
             for(int i = 0; i < s.Length; i++)
             {
-
+                if(uniqueCharacters.ContainsKey(s[i]))
+                {
+                    indexes.Remove(uniqueCharacters[s[i]]);
+                    uniqueCharacters[s[i]] = -1;
+                }
+                else
+                {
+                    uniqueCharacters.Add(s[i], i);
+                    indexes.Add(i);
+                }
             }
 
-            return uniquePointer;
+            if (indexes.Count == 0)
+                return -1;
+            return indexes[0];
+        }
+
+        public char RepeatedCharacter(string s)
+        {
+            HashSet<char> uniqueCharacters = new HashSet<char>();
+            foreach(char c in s)
+            {
+                if(!uniqueCharacters.Contains(c))
+                {
+                    uniqueCharacters.Add(c);
+                }
+                else
+                {
+                    return c;
+                }
+            }
+            return ' ';
+        }
+
+        public int LongestPalindrome(string s)
+        {
+            //add every character seen to a hash map
+            //add up all the hashes that are mod 2 and then if there is any with mod 1 add +1
+            Dictionary<char, int> countCharacters = new Dictionary<char, int>();
+            int countLongest = 0;
+            bool hasOdd = false;
+            foreach (char c in s)
+            {
+                if(countCharacters.ContainsKey(c))
+                {
+                    countCharacters[c]++;
+                }
+                else
+                {
+                    countCharacters.Add(c, 1);
+                }
+            }
+
+
+            foreach(var count in countCharacters.Values)
+            {
+                if(count >= 2)
+                {
+                    if (count % 2 == 0)
+                    {
+                        countLongest += count;
+                    }
+                    else
+                    {
+                        countLongest += count - 1;
+                        hasOdd = true;
+                    }
+                }
+                else
+                {
+                    hasOdd = true;
+                }
+            }
+
+
+            return hasOdd? ++countLongest : countLongest;
         }
     }
 }
