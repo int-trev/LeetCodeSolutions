2f6deff1e3fe78f343d660e53f6b12801ff2ca80
Trevor <trevor.lar@gmail.com>
2022-08-21 01:20:40 -0400

    Saving work in progress.

---
+++ AnotherLeetcodeProj/FirstUniqueCharInString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherLeetcodeProj
+{
+    internal class FirstUniqueCharInString
+    {
+        public int FirstUniqChar(string s)
+        {
+            int uniquePointer = 0;
+            int movingPointer = 0;
+            HashSet<char> chars = new HashSet<char>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+
+            }
+
+            return uniquePointer;
+        }
+    }
+}
---
+++ AnotherLeetcodeProj/HappyNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherLeetcodeProj
+{
+    internal class HappyNumber
+    {
+        public bool IsHappy(int n)
+        {
+            //immediately I think of a hash set
+            //save the values of all the ints that we use
+            //if we see those ints again, return false
+            //if we reach 1 sum, then true.
+            HashSet<int> happy = new HashSet<int>();
+
+            happy.Add(n);
+            while(true)
+            {
+                n = SumHelper(n);
+                if(n == 1)
+                {
+                    return true;
+                }
+                else if(happy.Contains(n))
+                {
+                    return false;
+                }
+                else
+                {
+                    happy.Add(n);
+                }
+            }
+        }
+
+        private int SumHelper(int num)
+        {
+            int sum = 0;
+            while(num > 0)
+            {
+                int digit = num % 10;
+                sum += digit * digit;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
